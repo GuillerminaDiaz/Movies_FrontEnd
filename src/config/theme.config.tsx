db5d217a56b6410167df3af40b845ce27ed3680a
guillerminaDiaz <guillerminaadiaz@gmail.com>
2023-08-06 22:17:48 -0300

    first commit

---
+++ src/config/theme.config.tsx
@@ -0,0 +1,47 @@
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
+import React from "react";
+
+type ThemeProp = {
+  children: JSX.Element;
+};
+
+export enum themePalette {
+  BG = "#0D152B",
+  LBLUE = "#32B4FF",
+  FONT_GLOBAL = "'JetBrains Mono', monospace",
+}
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: themePalette.BG,
+    },
+    primary: {
+      main: themePalette.LBLUE,
+    },
+  },
+  typography: {
+    fontFamily: themePalette.FONT_GLOBAL,
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        style: {
+          textTransform: "none",
+          boxShadow: "none",
+          borderRadius: "0.5em",
+        },
+      },
+    },
+  },
+});
+
+export const ThemeConfig: React.FC<ThemeProp> = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+};
